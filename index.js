b59be28e4d1f479a8a7e63bd740a11d898441368
Cori Cathemer <coricathemer@gmail.com>
2021-02-20 12:29:48 -0700

    first test

---
+++ index.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const inquirer = require('inquirer');
+
+// add input questiions here 
+const questiions {
+
+}
+
+// create function to write to file 
+
+// create function to initialize app 
+
+// init
