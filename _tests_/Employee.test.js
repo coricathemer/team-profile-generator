c6ea25fcef886d7d0e24280ff7d81d79b972f887
Cori Cathemer <coricathemer@gmail.com>
2021-02-20 15:36:42 -0700

    added index.html, style.cc, and bootstrap cdn

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -2,5 +2,5 @@ const { test, expect } = require('@jest/globals');
 const checkIfEqual = require('../lib/Employee');
 
 test('checks if 10 is equal to 10', () => {
-  expect(checkIfEqual(10,10).toBe(true));
+  expect(checkIfEqual(10, 10)).toBe(true);
 })
--- index.js
+++ index.js
@@ -2,9 +2,38 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 
 // add input questiions here 
-const questiions {
+inquirer
+  .prompt([
+    /* Pass your questions in here */
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    },
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your title within the company?'
+    },
+    {
+      type: 'list',
+      name: 'name',
+      message: 'How can you best be reached?',
+      choices: ['email', 'phone', 'linkedin', 'other']
 
-}
+    },
+    
+  ])
+  .then(answers => {
+    // Use user feedback for... whatever!!
+  })
+  .catch(error => {
+    if(error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else when wrong
+    }
+  });
 
 // create function to write to file 
 
